1792ffeefa5ec931ce809e830c4cf34ede83e7be
MahapatraA <anishm22@iitk.ac.in>
2026-04-10 01:01:34 +0530

    PORT changed

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/portfolio", portfolioRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
