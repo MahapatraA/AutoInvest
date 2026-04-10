79498ec19eaf89a770e381bfbeb83b9fdfe1052e
MahapatraA <anishm22@iitk.ac.in>
2026-04-11 02:45:18 +0530

    api changed

--- services/aiService.js
+++ services/aiService.js
@@ -15,7 +15,7 @@ const extractJSON = (text) => {
 exports.getAIResponse = async (message) => {
   try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           {
